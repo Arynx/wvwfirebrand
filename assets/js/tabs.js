ea08d46712b1dd48451c192e86e86fa139ce0709
Arynx <36913801+Arynx@users.noreply.github.com>
2018-09-08 17:30:19 -0400

    Initial commit

---
+++ assets/js/tabs.js
@@ -0,0 +1,16 @@
+function openBuild(buildName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(buildName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+
+document.getElementById("defaultOpen").click();
